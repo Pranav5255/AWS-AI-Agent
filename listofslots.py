1890e3be1bc297fdd456b4a2547445737a32bd98
Pranav5255 <76805382+Pranav5255@users.noreply.github.com>
2025-04-07 18:39:40 +0530

    Update listofslots.py

---
+++ listofslots.py
@@ -0,0 +1,31 @@
+
+import json
+
+def lambda_handler(event, context):
+    agent = event['agent']
+    actionGroup = event['actionGroup']
+    function = event['function']
+    parameters = event.get('parameters', [])
+
+    print("Looking for available slots...")
+
+    # Execute your business logic here. For more information, refer to: https://docs.aws.amazon.com/bedrock/latest/userguide/agents-lambda.html
+    responseBody =  {
+        "TEXT": {
+            "body": "The following slots are available: "
+        }
+    }
+
+    action_response = {
+        'actionGroup': actionGroup,
+        'function': function,
+        'functionResponse': {
+            'responseBody': responseBody
+        }
+
+    }
+
+    dummy_function_response = {'response': action_response, 'messageVersion': event['messageVersion']}
+    print("Response: {}".format(dummy_function_response))
+
+    return dummy_function_response
